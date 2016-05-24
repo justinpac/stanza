7be09341bf132519d1bdb65587c93553301b106f
Adam Kilbo <amkilbo@gmail.com>
2016-05-24 16:09:19 -0500

    Javadoc for CommInterface.java

--- Stanza/app/src/main/java/com/example/stanza/CommInterface.java
+++ Stanza/app/src/main/java/com/example/stanza/CommInterface.java
@@ -1,12 +1,24 @@
 package com.example.stanza;
 
 /**
- * Created by Brianna on 4/27/2016.
+ * A class that will help interact with the CommThread
  */
-
-//adding to git
 public interface CommInterface {
+    // methods
+
+    /**
+     * Displays a message when Pull is finished
+     */
     void onPullFinished();
+
+    /**
+     * Displays a message when the poem is saved to backend
+     * @param output the title of the poem that will be displayed when saved
+     */
     void poemSaved(String output);
+
+    /**
+     * Displays a message when the server is inaccessible
+     */
     void serverDisconnected();
 }
