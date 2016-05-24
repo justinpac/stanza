bfac6d84b53b6851dfbcf198d77b6e9eb5a8a5be
Adam Kilbo <amkilbo@gmail.com>
2016-05-24 12:37:37 -0500

    Javadoc is done for DBOpenHelper.java and NotesProvider.java

--- Stanza/app/src/main/java/com/example/stanza/NotesProvider.java
+++ Stanza/app/src/main/java/com/example/stanza/NotesProvider.java
@@ -9,42 +9,82 @@ import android.database.sqlite.SQLiteException;
 import android.net.Uri;
 import android.util.Log;
 
+/**
+ * A class for retrieving the notes from our table in the database
+ */
 public class NotesProvider extends ContentProvider{
-
+    // class variables
+    /**
+     * The program requesting the notes. Ensures correct permissions such that only this program
+     *      will access the table in <code>NotesProvider</code>
+     */
     private static final String AUTHORITY = "com.example.stanza.notesprovider";
-    //private static final String BASE_PATH = "notes";
+    /**
+     * The table we are looking at is the local database holding the poems on the device in <code>NotesProvider</code>
+     */
     private static final String BASE_PATH = "poemsLocal";
+    /**
+     * Constructs the Uri using AUTHORITY and BASE_PATH in <code>NotesProvider</code>
+     */
     public static Uri CONTENT_URI =
             Uri.parse("content://" + AUTHORITY + "/" + BASE_PATH );
- //   public static Uri CONTENT_URI2 =
-   //         Uri.parse("content://" + AUTHORITY + "/" + BASE_PATH );
 
-    // Constant to identify the requested operation
+    /**
+     * A constant used to identify the requested operation in <code>NotesProvider</code>
+     */
     private static final int NOTES = 1;   //get data
+    /**
+     * A constant used to intify the requested operation in <code>NotesProvider</code>
+     */
     private static final int NOTES_ID = 2;  //deals with only single record
 
+    /**
+     * A constant used to match Uri's in <code>NotesProvider</code>
+     */
     private static final UriMatcher uriMatcher =
             new UriMatcher(UriMatcher.NO_MATCH);
 
+    /**
+     * A constant used to identify the content type in <code>NotesProvider</code>
+     */
     public static final String CONTENT_ITEM_TYPE = "Poem";
 
+    /**
+     * Updates uriMatcher with arguments in <code>NotesProvider</code>
+     */
     static {
         uriMatcher.addURI(AUTHORITY, BASE_PATH, NOTES);
         uriMatcher.addURI(AUTHORITY, BASE_PATH +  "/#", NOTES_ID);
     }
 
+    /**
+     * The database which we are using to store our table in <code>NotesProvider</code>
+     */
     private SQLiteDatabase database;
 
+    //methods
+
+    /**
+     *  Gets the context and hence correct table with our information
+     * @return true
+     */
     @Override
     public boolean onCreate() {
 
         DBOpenHelper helper = new DBOpenHelper(getContext());
         database = helper.getWritableDatabase();
-       // CONTENT_URI = CONTENT_URI.buildUpon().appendQueryParameter("limit", "1000").build();
-      //  CONTENT_URI2 = CONTENT_URI2.buildUpon().appendQueryParameter("limit", "10").build();
         return true;
     }
 
+    /**
+     * Allows for dynamic access to the database based on cursor position.
+     * @param uri identifies the table.
+     * @param projection ???
+     * @param selection The text selected???
+     * @param selectionArgs the text that is selected???
+     * @param sortOrder How we want the return to be sorted
+     * @return The database query results
+     */
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
 
@@ -58,36 +98,54 @@ public class NotesProvider extends ContentProvider{
         }
     }
 
-
+    /**
+     * A required method for making a notesProvider, does nothing functionally.
+     * @param uri identifies the table
+     * @return null
+     */
     @Override
     public String getType(Uri uri) {
         return null;
     }
 
+    /**
+     * What we want to insert into the database
+     * @param uri identifies the table
+     * @param values What is being inserted into the table
+     * @return the resultant table identification after insertion
+     */
     @Override
     public Uri insert(Uri uri, ContentValues values) {
         long id = database.insert(DBOpenHelper.TABLE_POEMS,
                 null, values);
         Uri result;
-
-       // System.out.println(id);
-
-       // System.out.println(Uri.parse(BASE_PATH + "/" + id));
         result = Uri.parse(BASE_PATH + "/" + id);
-       // System.out.println("in insert " + result.getLastPathSegment());
         return result;
 
     }
 
+    /**
+     * Delete elements from the table
+     * @param uri the table identifier
+     * @param selection The selected element(s) to delete
+     * @param selectionArgs Arguments wanted in the deletion process
+     * @return The database with the deleted element(s)
+     */
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
         return database.delete(DBOpenHelper.TABLE_POEMS, selection, selectionArgs);
     }
 
+    /**
+     * Updates the table after some change
+     * @param uri The table identifier
+     * @param values The values to be updated into the table.
+     * @param selection The selection to be updated.
+     * @param selectionArgs Possible args that change how we update.
+     * @return the database after the update operation.
+     */
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        //selection = "_id=" + uri.getLastPathSegment();
-        //System.out.println("update selection is " + selection);
         return database.update(DBOpenHelper.TABLE_POEMS,
                 values, selection, selectionArgs);
     }
